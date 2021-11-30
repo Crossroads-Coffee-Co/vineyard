9d4024720987e8b60cbfe839fb6c082af36879a6
mwforrest7 <mwforrest7@gmail.com>
2021-11-29 19:07:44 -0700

    added skeleton class

---
+++ src/main/java/net/mwforrest7/vineyard/block/custom/VineStemBlock.java
@@ -0,0 +1,10 @@
+package net.mwforrest7.vineyard.block.custom;
+
+import net.minecraft.block.CropBlock;
+import net.minecraft.block.PlantBlock;
+
+public class VineStemBlock extends PlantBlock {
+    protected VineStemBlock(Settings settings) {
+        super(settings);
+    }
+}
