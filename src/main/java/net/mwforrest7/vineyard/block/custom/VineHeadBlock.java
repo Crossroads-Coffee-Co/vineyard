3a67f014802b83c497af298226cd7a9daa2d6b04
mwforrest7 <mwforrest7@gmail.com>
2021-12-16 19:08:25 -0700

    Added more custom assets

--- src/main/java/net/mwforrest7/vineyard/block/custom/VineHeadBlock.java
+++ src/main/java/net/mwforrest7/vineyard/block/custom/VineHeadBlock.java
@@ -3,6 +3,8 @@ package net.mwforrest7.vineyard.block.custom;
 import net.minecraft.block.*;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
@@ -15,6 +17,8 @@ import static net.mwforrest7.vineyard.util.VineUtil.isAlongFence;
 public class VineHeadBlock extends CropBlock {
     private final VineCanopyBlock vineCanopyBlock;
     private final AttachedVineTrunkBlock attachedVineTrunkBlock;
+    public static final int MAX_AGE = Properties.AGE_1_MAX;
+    public static final IntProperty AGE = Properties.AGE_1;
 
     public VineHeadBlock(VineCanopyBlock vineCanopyBlock, AttachedVineTrunkBlock attachedVineTrunkBlock, AbstractBlock.Settings settings) {
         super(settings);
@@ -33,23 +37,39 @@ public class VineHeadBlock extends CropBlock {
         // If sufficient moisture...
         float f = CropBlock.getAvailableMoisture(this.attachedVineTrunkBlock, world, pos.down());
         if (random.nextInt((int)(25.0f / f) + 1) == 0) {
+            System.out.println("Age is: " + state.get(AGE));
 
             if (!isMature(state)) {
+                System.out.println("is not mature... aging up");
                 // If immature - age up
                 state = state.with(AGE, state.get(AGE) + 1);
                 world.setBlockState(pos, state, Block.NOTIFY_LISTENERS);
-            } else {
+            }else {
+                System.out.println("Is mature.");
                 // Else if mature, grow vine canopies in all horizontal directions where there is open space & a fence
                 for (Direction direction : Direction.Type.HORIZONTAL) {
                     BlockPos adjacentBlock = pos.offset(direction);
                     if (world.getBlockState(adjacentBlock).isAir() && isAlongFence(world, adjacentBlock)) {
+                        System.out.println("Adjacent block is air and along fence. Setting to canopy.");
                         world.setBlockState(adjacentBlock, this.vineCanopyBlock.getDefaultState());
                     }
                 }
                 // After canopies are grown, turn into an attached head block
+                System.out.println("Canopies set. Changing this block to attached head.");
                 world.setBlockState(pos, this.vineCanopyBlock.getAttachedHeadBlock().getDefaultState());
+                System.out.println("Finished.");
             }
         }
+    }
+
+    @Override
+    public IntProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
     }
 
     @Override
