1549fb0e0b7221bb4cfbd59a0dc83580e1514329
mwforrest7 <mwforrest7@gmail.com>
2021-12-24 16:23:54 -0700

    Added grape juice

---
+++ src/main/java/net/mwforrest7/vineyard/item/GrapeJuiceBottleItem.java
@@ -0,0 +1,76 @@
+package net.mwforrest7.vineyard.item;
+
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+public class GrapeJuiceBottleItem extends Item {
+    private static final int MAX_USE_TIME = 40;
+
+    public GrapeJuiceBottleItem(Settings settings) {
+        super(settings);
+    }
+
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        super.finishUsing(stack, world, user);
+        if (user instanceof ServerPlayerEntity) {
+            ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)user;
+            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+
+        if (stack.isEmpty()) {
+            return new ItemStack(Items.GLASS_BOTTLE);
+        } else {
+            if (user instanceof PlayerEntity && !((PlayerEntity)user).getAbilities().creativeMode) {
+                ItemStack serverPlayerEntity = new ItemStack(Items.GLASS_BOTTLE);
+                PlayerEntity playerEntity = (PlayerEntity)user;
+                if (!playerEntity.getInventory().insertStack(serverPlayerEntity)) {
+                    playerEntity.dropItem(serverPlayerEntity, false);
+                }
+            }
+
+            return stack;
+        }
+    }
+
+    public int getMaxUseTime(ItemStack stack) {
+        return MAX_USE_TIME;
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.DRINK;
+    }
+
+    public SoundEvent getDrinkSound() {
+        return SoundEvents.ENTITY_GENERIC_DRINK;
+    }
+
+    public SoundEvent getEatSound() {
+        return SoundEvents.ENTITY_GENERIC_DRINK;
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (this.isFood()) {
+            ItemStack itemStack = user.getStackInHand(hand);
+            if (user.canConsume(this.getFoodComponent().isAlwaysEdible())) {
+                user.setCurrentHand(hand);
+                return TypedActionResult.consume(itemStack);
+            } else {
+                return TypedActionResult.fail(itemStack);
+            }
+        } else {
+            return TypedActionResult.pass(user.getStackInHand(hand));
+        }
+    }
+
+}
--- src/main/java/net/mwforrest7/vineyard/item/ModItems.java
+++ src/main/java/net/mwforrest7/vineyard/item/ModItems.java
@@ -7,6 +7,8 @@ import net.minecraft.util.registry.Registry;
 import net.mwforrest7.vineyard.VineyardMod;
 import net.mwforrest7.vineyard.block.ModBlocks;
 
+import static net.minecraft.item.Items.GLASS_BOTTLE;
+
 public class ModItems {
 
     // Initialization of items
@@ -19,6 +21,8 @@ public class ModItems {
             new Item(new FabricItemSettings()
                     .food(new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build())
                     .group(ModItemGroups.VINEYARD)));
+
+    public static final Item RED_GRAPE_JUICE_BOTTLE = registerItem("red_grape_juice_bottle", new GrapeJuiceBottleItem((new FabricItemSettings()).recipeRemainder(GLASS_BOTTLE).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build()).group(ModItemGroups.VINEYARD).maxCount(16)));
 
     public static final Item RED_GRAPE_SEEDS = registerItem("red_grape_seeds",
             new AliasedBlockItem(ModBlocks.RED_GRAPEVINE_TRUNK,
