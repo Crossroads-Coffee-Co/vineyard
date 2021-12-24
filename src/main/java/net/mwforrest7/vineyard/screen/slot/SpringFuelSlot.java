07351f9daad13adff547a30cf5a01d6d82165809
mwforrest7 <mwforrest7@gmail.com>
2021-12-24 11:14:35 -0700

    Removed debug logging. Also, renamed the fuel slot to be more accurate.

--- src/main/java/net/mwforrest7/vineyard/screen/slot/SpringFuelSlot.java
+++ src/main/java/net/mwforrest7/vineyard/screen/slot/SpringFuelSlot.java
@@ -1,17 +1,15 @@
 package net.mwforrest7.vineyard.screen.slot;
 
-import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.screen.slot.Slot;
 import net.mwforrest7.vineyard.item.ModItems;
 
 /**
  * Enforces this inventory slot to be used only by fuels
  */
-public class ModFuelSlot extends Slot {
-    public ModFuelSlot(Inventory inventory, int index, int x, int y) {
+public class SpringFuelSlot extends Slot {
+    public SpringFuelSlot(Inventory inventory, int index, int x, int y) {
         super(inventory, index, x, y);
     }
 
