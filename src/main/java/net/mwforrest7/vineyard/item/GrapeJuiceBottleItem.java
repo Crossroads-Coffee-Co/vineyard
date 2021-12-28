e20f56902a06894fd427e77e78b6901e0fd9d88f
mwforrest7 <mwforrest7@gmail.com>
2021-12-28 10:33:25 -0700

    Documented and cleaned up GrapeJuiceBottle

--- src/main/java/net/mwforrest7/vineyard/item/GrapeJuiceBottleItem.java
+++ src/main/java/net/mwforrest7/vineyard/item/GrapeJuiceBottleItem.java
@@ -8,32 +8,43 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
 public class GrapeJuiceBottleItem extends Item {
+    // Time it takes to consume the juice
     private static final int MAX_USE_TIME = 40;
 
     public GrapeJuiceBottleItem(Settings settings) {
         super(settings);
     }
 
+    /**
+     * This function is executed when the item is finished being used
+     */
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         super.finishUsing(stack, world, user);
-        if (user instanceof ServerPlayerEntity) {
-            ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)user;
+
+        // The ServerPlayerEntity handles all the player data
+        if (user instanceof ServerPlayerEntity serverPlayerEntity) {
+            // This appears to be related to triggering advancements - is not necessary
             Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+
+            // Increments relevant player stats - also not necessary
             serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
         }
 
+        // If the item stack in hand is empty, put an empty glass bottle there
         if (stack.isEmpty()) {
             return new ItemStack(Items.GLASS_BOTTLE);
-        } else {
+        }
+        // Else, if player is not in creative mode
+        else {
             if (user instanceof PlayerEntity && !((PlayerEntity)user).getAbilities().creativeMode) {
                 ItemStack serverPlayerEntity = new ItemStack(Items.GLASS_BOTTLE);
                 PlayerEntity playerEntity = (PlayerEntity)user;
+
+                // If there is no space to insert the bottles into player inventory, drop on ground
                 if (!playerEntity.getInventory().insertStack(serverPlayerEntity)) {
                     playerEntity.dropItem(serverPlayerEntity, false);
                 }
@@ -47,30 +58,18 @@ public class GrapeJuiceBottleItem extends Item {
         return MAX_USE_TIME;
     }
 
+    // Determines what type of action it is when using this item
     public UseAction getUseAction(ItemStack stack) {
         return UseAction.DRINK;
     }
 
+    // Determines the sound when performing a Drink action
     public SoundEvent getDrinkSound() {
         return SoundEvents.ENTITY_GENERIC_DRINK;
     }
 
+    // Determines the sound made when finished consuming the item
     public SoundEvent getEatSound() {
         return SoundEvents.ENTITY_GENERIC_DRINK;
     }
-
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (this.isFood()) {
-            ItemStack itemStack = user.getStackInHand(hand);
-            if (user.canConsume(this.getFoodComponent().isAlwaysEdible())) {
-                user.setCurrentHand(hand);
-                return TypedActionResult.consume(itemStack);
-            } else {
-                return TypedActionResult.fail(itemStack);
-            }
-        } else {
-            return TypedActionResult.pass(user.getStackInHand(hand));
-        }
-    }
-
 }
